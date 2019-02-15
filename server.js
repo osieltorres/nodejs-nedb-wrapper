e401541ec150a48f94c0996160dc3db656d9698f
osieltorres <torres89bz@gmail.com>
2019-02-15 15:23:22 -0500

    Encode JSON requests

--- server.js
+++ server.js
@@ -6,5 +6,13 @@ let parser = require('body-parser');
 // import NeDB database
 const db = require('./store');
 
+// to support JSON-encoded bodies.
+app.use(parser.json());
+
+// to support URL-encoded bodies
+app.use(parser.urlencoded({
+  extended: true
+}));
+
 // set app port for connections to listen
 app.listen(4500, () => console.log('Listening on port 4500'));
