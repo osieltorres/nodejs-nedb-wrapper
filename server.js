ed55a54d13902ceab8e48ff4ae7e0034137cfff9
osieltorres <torres89bz@gmail.com>
2019-02-15 15:18:29 -0500

    Set database variable to manage transactions

--- server.js
+++ server.js
@@ -3,5 +3,8 @@ let express = require("express");
 let app = express();
 let parser = require('body-parser');
 
+// import NeDB database
+const db = require('./store');
+
 // set app port for connections to listen
 app.listen(4500, () => console.log('Listening on port 4500'));
